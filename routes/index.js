5939e28f9b07578482887cd7f3da712f12533706
Stéphane Dauguet <sdauguet@gmail.com>
2021-11-10 12:09:18 +0100

    list tweets

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const router = require("express").Router();
 const api = require("./api");
+const Tweet = require("../database/models/Tweet");
 
 router.use("/api", api);
 
 router.get("/", (req, res) => {
-  res.render("tweets/tweets-list");
+  Tweet.find({})
+    .exec()
+    .then((tweets) => {
+      res.render("tweets/tweets-list", { tweets });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send(err);
+    });
 });
 
 router.get("/tweet/new", (req, res) => {
